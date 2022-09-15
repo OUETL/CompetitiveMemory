cfed39a4e804a7594dbb19bb6c5fc98b9f9302dc
Bobby Reed <37113385+bobbyreed@users.noreply.github.com>
2022-09-15 08:20:28 -0500

    init commit

---
+++ CompetitiveMemory/CompetitiveMemory/Assets/Scripts/Board.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Transform[] spawnLocations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //need for loop that puts a random card on every spawnLocation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
